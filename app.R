59aff7f4105c1957b7f55f7862d28523fee9f9a4
Jérémy Rousseau <jeremy.rousseau2@outlook.fr>
2024-04-26 18:30:02 +0200

    Update app.R

--- app.R
+++ app.R
@@ -6,48 +6,71 @@ library(shinythemes)
 
 ui = tagList(
   
-  #shinythemes::themeSelector(),
-  
   navbarPage(
-    #theme = "cerulean",  # <--- To use a theme, uncomment this
+    
     theme = shinytheme("cerulean"),
     
     "LAGOON-MCL ",
-    tabPanel("Homogeneity score - distribution",
+    
+    tabPanel("Homogeneity score",
              
              sidebarPanel(
+          
+               
                fileInput("file1", "File input:", 
                          accept = c("text/tsv",
                                     "test/tab-separated-value,text/plain",
                                     ".tsv")),
-               #textInput("txt", "Text input:", "general"),
-               numericInput("sliderBins", "Bins:", min = 1, value = 50, 
-                            step = 1),
-               sliderInput("sliderHomSc", "homogeneiry score:", 0, 1,
-                           value = c(0.0, 1.0)),
-             ),
+               
+               #uiOutput("columnY"),
+               
+               fluidRow(radioButtons("plot_type", "Plot type:",
+                                     list("Histogram", "Boxplot", "Point")),
+                       
+                        conditionalPanel(condition="input.plot_type=='Histogram'",
+                                         uiOutput("columnX"),
+                                         numericInput("sliderBins", "Bins:",
+                                                      min = 1, value = 50,
+                                                      step = 1),
+                                         sliderInput("sliderHomSc", 
+                                                     "homogeneiry score:", 0, 1,
+                                                     value = c(0.0, 1.0))
+                                         ),
+                        ),
+               ),
              
              mainPanel(
                
                tabsetPanel(type = "tabs",
                            tabPanel("Summary", tableOutput("summary")),
-                           tabPanel("Plot", plotOutput("plot")),
-                           tabPanel("Table", tableOutput("table"))),
+                           tabPanel("Plot",
+                                    conditionalPanel(condition="input.plot_type
+                                                     =='Histogram'",
+                                                     plotOutput("histo")),
+                                    conditionalPanel(condition="input.plot_type
+                                                     =='Boxplot'",
+                                                     plotOutput("")
+                                    ),
+                                    conditionalPanel(condition="input.plot_type
+                                                     =='Point'",
+                                                     plotOutput("")
+                                    )
+                                    
+                                    
+                                    ),
+                           tabPanel("Table", tableOutput("table")),
+                           tabPanel("Cluster selection")),
              )
     ),
     
-    tabPanel("Cluster size - distribution", "In development",
-             sidebarPanel(
-               fileInput("file2", "File input:"), 
-               numericInput("inMin", "Minimum cluster size:", min = 0, 
-                            value = 0, step = 1),
-               numericInput("inMax", "Maximum cluster size:", min = 0, 
-                            value = 10, step = 1)
-             )),
-    
-    tabPanel("Homogeneity score - Cluster size", "In development",
-             sidebarPanel(
-               fileInput("file2", "File input:"))),
+#    tabPanel("Cluster size", "In development",
+#             sidebarPanel(
+#               fileInput("file2", "File input:"), 
+#               numericInput("inMin", "Minimum cluster size:", min = 0, 
+#                            value = 0, step = 1),
+#               numericInput("inMax", "Maximum cluster size:", min = 0, 
+#                            value = 10, step = 1)
+#             )),
     
     tabPanel("About", "Cette applicaiton shiny permet d'explorer de manière 
              intéractive les différents fichiers TSV généré par le pipeline
@@ -57,46 +80,97 @@ ui = tagList(
 
 
 
-server = function(input, output) {
+server = function(input, output, session) {
   
   
   filedata1 <- reactive({
     
     infile <- input$file1
-    if (is.null(infile)){
-      return(NULL)      
+    if (is.null(infile)) {
+      return(NULL)
     }
+    #paste("data/", infile$datapath, sep = "", collapse)
     read.table(infile$datapath, header = TRUE, sep = "\t")
   })
-  
-  output$table <- renderTable({
-    
+
+  output$columnX <- renderUI({
     dataframe <- filedata1()
-    head(dataframe, n = 40)
+    
+    if (is.null(dataframe)) return(NULL)
+    
+    nums <- sapply(dataframe, is.numeric)
+    
+    items = names(nums[nums])
+    
+    names(items) = items
+    
+    selectInput("Xcol", "Axis X", items)
   })
+  
+  output$columnY <- renderUI({
+    dataframe <- filedata1()
+    
+    if (is.null(dataframe)) return(NULL)
+    
+    nums <- sapply(dataframe, is.numeric)
+    
+    items = names(nums[nums])
+    
+    names(items) = items
+    
+    selectInput("Ycol", "Axis Y", items, selected = dataframe$columnY)
+  })
+  
+
   
   output$summary <- renderText({
     dataframe <- filedata1()
     summary(dataframe)
   })
   
-  output$plot <- renderPlot({
+  column <- reactive({
+    input$Xcol
+  })
+  
+  filedata1filter <- reactive({
+    
+    column <- column()
     
     dataframe <- filedata1()
     
+    #x_column <- names(input$Xcol)
+    #x_column <- names(items)[items == input$Xcol]
+    min_homsc <- input$sliderHomSc[1]
+    max_homsc <- input$sliderHomSc[2]
+    
+    df_filter <- dataframe %>% 
+      filter(get(column) >= min_homsc & get(column) <= max_homsc)
+    
+    df_filter
+  })
+  
+  output$table <- renderTable({
+    
+    dataframe <- filedata1filter()
+    head(dataframe, n = 40)
+  })
+  
+  output$histo <- renderPlot({
+    #dataframe <- filedata1()
+    dataframe <- filedata1filter()
+    
+
     graph <- dataframe %>%
-      filter(homogeneity_score >= input$sliderHomSc[1] &
-               homogeneity_score <= input$sliderHomSc[2]) %>% 
-      ggplot(aes(x = homogeneity_score)) +
+      ggplot(aes(x = dataframe[,input$Xcol])) +
       geom_histogram(bins = input$sliderBins, color = "darkblue", 
-                     fill = "lightblue") +
-      theme_light() +
-      labs(title = "title_labs",
-           x = "x_labs",
-           y = "Number of clusters") +
-      theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
-            axis.title = element_text(size = 14),
-            axis.text = element_text(size = 12))
+                     fill = "lightblue") 
+      #theme_light() +
+      #labs(title = "title_labs",
+      #     x = "x_labs",
+      #     y = "Number of clusters") +
+      #theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+      #      axis.title = element_text(size = 14),
+      #      axis.text = element_text(size = 12))
     graph
   })
 }
